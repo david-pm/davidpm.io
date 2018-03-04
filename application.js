17f69223a561146572757000bbf6861e9237e74f
david-pm <damcdonald@weedmaps.com>
2018-03-03 22:12:36 -0800

    initial commit - static page and assets

---
+++ application.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  const myList = [
+    "go to school buy a mac get good grades buy a car go to work",
+    "fintech relo flexible hours work from home 401k Roth-IRA",
+    "GMO PPO IPO C3PO FOMO YOLO UI UX HMO BB8 CRM CMS PX CEM ICO",
+    "do the dew i'm lovin' it it's miller time celebrate football",
+    "gatorade monster rockstar redbull powerade vitamin water",
+    "BTC XMR GVT EOS ETH QSP LSK VEN NEO LTC BCC ICX XLM",
+    "cyber warriors hackers nation state spies crackers script kiddies",
+    "have kids early save for the future plan ahead watch the news",
+    "costco sams club kmart walmart target jcpenny sears kohls", 
+    "the war on drugs palestine sida runaway greenhouse effect",
+    "find a hobby pay for cable water your lawn be a good neighbor"
+  ]
+ let rand = myList[Math.floor(Math.random() * myList.length)];
+ $('#mantra')[0].innerHTML = "> " + rand;
+
+  var menu = $('#menu');
+  $('#pattern').on({
+    mouseenter : function() {
+      menu.fadeToggle(300);
+      return false;
+    },
+    mouseleave : function() {
+      menu.fadeToggle(300);
+      return false;
+    },
+    click : function() {
+      if ( menu.css('display') === 'none' ) {
+        menu.fadeIn(300);
+      }
+    }
+  });
+});
